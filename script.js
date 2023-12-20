ff9987a1ea8cedfcdd93cd3fa52d209303a7605a
ALBANO-Coline-2326001 <coline.albano@etu.univ-amu.fr>
2023-12-20 12:13:16 +0100

    test

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-document.getElementById("theme").addEventListener("click", function() {
-    css=document.getElementById("css");
-    if (css.href.endsWith("styledark.css")) {
-        css.href="styleclair.css";
-    } else {
-        css.href="styledark.css";
-    }
-});
-
-// function titi() {
-//     let p = document.getElementById("p")
-//     let number1 = prompt("Entrez le premier nombre")
-//     let number2 = prompt("Entrez le deuxième nombre")
-//     let temp = number1 * number1 + number2 * number2
-//     let result = Math.sqrt(temp)
-//     p.innerHTML = result
-// }
+let buttons = document.querySelectorAll(".theme");
+console.log(buttons)
+buttons.forEach((elem) => {
+    elem.addEventListener("click", function() {
+        let css = document.getElementById("css");
+        if (css.href.endsWith("styledark.css")) {
+            css.href="styleclair.css";
+        } else {
+            css.href="styledark.css";
+        }
+    });
+})
