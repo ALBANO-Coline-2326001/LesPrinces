3bf5c04913fe0801c7c0d08ce5e37e12072b947c
ALBANO-Coline-2326001 <coline.albano@etu.univ-amu.fr>
2024-01-10 12:07:49 +0100

    test

--- menu.js
+++ menu.js
@@ -2,14 +2,14 @@ const menuHamburger = document.querySelector('.menu-hamburger')
 const navLinks = document.querySelector('.nav-links')
 
 menuHamburger.addEventListener('click', () => {
-navLinks.classList.toggle('mobile-menu')
+    navLinks.classList.toggle('mobile-menu')
 });
 
-document.getElementById("theme").addEventListener("click", function() {
-    css=document.getElementById("css");
+document.getElementById("theme").addEventListener("click", function () {
+    css = document.getElementById("css");
     if (css.href.endsWith("styledark.css")) {
-        css.href="styleclair.css";
+        css.href = "styleclair.css";
     } else {
-        css.href="styledark.css";
+        css.href = "styledark.css";
     }
 });
